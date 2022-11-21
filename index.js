fc9be2d2ec3d30c682aa46ed23e7abdc9b48ee1b
Luiz Felipe Prado Borba <luis.p.borba@hotmail.com>
2022-11-21 17:35:19 -0300

    Adição do card register

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ function login(){
     document.getElementById("card").style.visibility="visible"
     document.getElementById("card").style.zIndex = "999"
     document.getElementById("card").style.filter = "blur(0px)"
+    document.getElementById("card").style.transition="all 0.2s"
     //document.body.style.backgroundColor = "rgba(0, 0, 0, 0.8)"
     document.getElementById("todo").style.filter= "blur(10px)"
     
 }
+function btnRegister (){
+    document.getElementById("Register").style.visibility="visible"
+    document.getElementById("Register").style.zIndex = "999"
+    document.getElementById("Register").style.transition="all 0.2s"
+    document.getElementById("Register").style.filter = "blur(0px)"
+    //document.body.style.backgroundColor = "rgba(0, 0, 0, 0.8)"
+    document.getElementById("todo").style.filter= "blur(10px)"
+}
+function btnFecharRegister(){
+    document.getElementById("Register").style.visibility="hidden"
+    document.getElementById("Register").style.transition="all 0.2s"
+    document.body.style.background="none"
+    document.getElementById("todo").style.filter= "blur(0px)"
+}
+
 function fecharLogin(){
+    document.getElementById("card").style.transition="all 0.2s"
     document.getElementById("card").style.visibility="hidden"
     document.body.style.background="none"
     document.getElementById("todo").style.filter= "blur(0px)"
@@ -25,7 +42,5 @@ function btnClose(){
     document.getElementById("btnMobile").style.visibility = "hidden"
     document.getElementById("btnMobile").style.transition = "all 0.5s"
     document.getElementById("todo").style.backgroundColor = "rgba(0, 0, 0, 0)"
-    
-
 }
 

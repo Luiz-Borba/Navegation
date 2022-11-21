6f2ee2c2d4930031ac268a12ec16f61e87207b16
Luiz Felipe Prado Borba <luis.p.borba@hotmail.com>
2022-11-20 22:03:05 -0300

    primiero botão mobile

--- index.js
+++ index.js
@@ -11,4 +11,21 @@ function fecharLogin(){
     document.body.style.background="none"
     document.getElementById("todo").style.filter= "blur(0px)"
 }
+function btnAbrir(){
+    document.getElementById("btnMobile").style.height="100%"
+    document.getElementById("btnMobile").style.visibility = "visible"
+    document.getElementById("btnMobile").style.transition = "all 0.5s"
+    //document.getElementById("todo").style.backgroundColor = "rgba(0, 0, 0, 0.8)"
+    document.getElementById("todo").style.backgroundColor = "rgba(0, 0, 0, 0.9)"
+    document.getElementById('mobile').style.opacity = "0.0,8"
+    
+}
+function btnClose(){
+    document.getElementById("btnMobile").style.height="0%"
+    document.getElementById("btnMobile").style.visibility = "hidden"
+    document.getElementById("btnMobile").style.transition = "all 0.5s"
+    document.getElementById("todo").style.backgroundColor = "rgba(0, 0, 0, 0)"
+    
+
+}
 

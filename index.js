967c8cac46efc0eb53de6e3424cd074d39842306
Luiz Felipe Prado Borba <luis.p.borba@hotmail.com>
2022-12-01 18:57:07 -0300

    Frist header

--- index.js
+++ index.js
@@ -1,46 +1,7 @@
-function login(){
-    document.getElementById("card").style.visibility="visible"
-    document.getElementById("card").style.zIndex = "999"
-    document.getElementById("card").style.filter = "blur(0px)"
-    document.getElementById("card").style.transition="all 0.2s"
-    //document.body.style.backgroundColor = "rgba(0, 0, 0, 0.8)"
-    document.getElementById("todo").style.filter= "blur(10px)"
+function abrirSubmenu(){
+    document.getElementById("subMenu").style.visibility="visible"
+}
+function fecharMenu(){
+    document.getElementById("subMenu").style.visibility="hidden"
     
 }
-function btnRegister (){
-    document.getElementById("Register").style.visibility="visible"
-    document.getElementById("Register").style.zIndex = "999"
-    document.getElementById("Register").style.transition="all 0.2s"
-    document.getElementById("Register").style.filter = "blur(0px)"
-    //document.body.style.backgroundColor = "rgba(0, 0, 0, 0.8)"
-    document.getElementById("todo").style.filter= "blur(10px)"
-}
-function btnFecharRegister(){
-    document.getElementById("Register").style.visibility="hidden"
-    document.getElementById("Register").style.transition="all 0.2s"
-    document.body.style.background="none"
-    document.getElementById("todo").style.filter= "blur(0px)"
-}
-
-function fecharLogin(){
-    document.getElementById("card").style.transition="all 0.2s"
-    document.getElementById("card").style.visibility="hidden"
-    document.body.style.background="none"
-    document.getElementById("todo").style.filter= "blur(0px)"
-}
-function btnAbrir(){
-    document.getElementById("btnMobile").style.height="100%"
-    document.getElementById("btnMobile").style.visibility = "visible"
-    document.getElementById("btnMobile").style.transition = "all 0.5s"
-    //document.getElementById("todo").style.backgroundColor = "rgba(0, 0, 0, 0.8)"
-    document.getElementById("todo").style.backgroundColor = "rgba(0, 0, 0, 0.9)"
-    document.getElementById('mobile').style.opacity = "0.0,8"
-    
-}
-function btnClose(){
-    document.getElementById("btnMobile").style.height="0%"
-    document.getElementById("btnMobile").style.visibility = "hidden"
-    document.getElementById("btnMobile").style.transition = "all 0.5s"
-    document.getElementById("todo").style.backgroundColor = "rgba(0, 0, 0, 0)"
-}
-

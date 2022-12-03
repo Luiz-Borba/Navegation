2f6145de4a6589702009fc4e34a22b08f4b2e91c
Luiz Felipe Prado Borba <luis.p.borba@hotmail.com>
2022-12-03 09:45:14 -0300

    menu dropDown

--- index.js
+++
@@ -1,7 +0,0 @@
-function abrirSubmenu(){
-    document.getElementById("subMenu").style.visibility="visible"
-}
-function fecharMenu(){
-    document.getElementById("subMenu").style.visibility="hidden"
-    
-}
